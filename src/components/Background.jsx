707079b0f488eb164948ba15111a8d2be84852d1
derricklai33 <derricklai33@hotmail.com>
2021-03-09 11:21:53 +1100

    Background component with css

---
+++ src/components/Background.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Background() {
+  return(
+    <>
+      <div id='stars'></div>
+      {/* <div id='stars2'></div> */}
+      <div id='stars3'></div>
+    </>
+  );
+}

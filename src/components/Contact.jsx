f75922cb6a4c2165226a3b7533c6a6f8d60d4e26
derricklai33 <derricklai33@hotmail.com>
2021-03-09 11:22:55 +1100

    Updated home/landing page component and updated placeholder contact me component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Contact() {
+  return(
+    <div className="page-divs" id="contact">
+      <h1>Contact Me</h1>
+    </div>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/app.scss'
+
+export function Home() {
+  return(
+    <div className="page-divs" id="home">
+      <h1 id="title">DERRICK LAI <br /><span>Full-Stack Developer</span></h1>
+    </div>
+  );
+}
